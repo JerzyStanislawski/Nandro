210ba3eefe40c09709b37c2054bd611bac428578
Stanislawski <stanislawski.jerzy@hotmail.com>
2021-04-09 07:06:29 +0200

    update

--- Nandro/App.axaml.cs
+++ Nandro/App.axaml.cs
@@ -1,9 +1,14 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using DotNano.RpcApi.Responses;
+using DotNano.Shared.DataTypes;
 using Nandro.ViewModels;
 using Nandro.Views;
+using System.Collections.Generic;
+using System.Numerics;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Nandro
 {
@@ -19,12 +24,24 @@ namespace Nandro
 
             _mainWindowVM.DisplayQR();
 
-            using var socket = new NanoSocket();
-            if (socket.Subscribe("wss://socket.nanos.cc", "nano_34prihdxwz3u4ps8qjnn14p7ujyewkoxkwyxm3u665it8rg5rdqw84qrypzk"))
+            var settings = new JsonSerializerOptions
             {
-                var response = socket.Listen();
-                _mainWindowVM.Block = JsonSerializer.Serialize(response, new JsonSerializerOptions { WriteIndented = true});
-            }
+                PropertyNameCaseInsensitive = true, NumberHandling = JsonNumberHandling.AllowReadingFromString
+            };
+            settings.Converters.Add(new PublicAddressConverter());
+            settings.Converters.Add(new BigIntegerConverter());
+            settings.Converters.Add(new HexKey64Converter());
+
+            using var apiClient = new NanoApiClient("https://proxy.nanos.cc/proxy");
+            var response = apiClient.AccountHistory("nano_34prihdxwz3u4ps8qjnn14p7ujyewkoxkwyxm3u665it8rg5rdqw84qrypzk");
+            var history = JsonSerializer.Deserialize<AccountHistoryResponse>(response, settings);
+
+            //using var socket = new NanoSocket();
+            //if (socket.Subscribe("wss://socket.nanos.cc", "nano_34prihdxwz3u4ps8qjnn14p7ujyewkoxkwyxm3u665it8rg5rdqw84qrypzk"))
+            //{
+            //    var response = socket.Listen();
+            //    _mainWindowVM.Block = JsonSerializer.Serialize(response, new JsonSerializerOptions { WriteIndented = true});
+            //}
         }
 
         public override void OnFrameworkInitializationCompleted()
@@ -38,6 +55,47 @@ namespace Nandro
             }
 
             base.OnFrameworkInitializationCompleted();
+        }
+
+        
+    }
+
+    class PublicAddressConverter : JsonConverter<PublicAddress>
+    {
+        public override PublicAddress? Read(ref Utf8JsonReader reader, System.Type typeToConvert, JsonSerializerOptions options)
+        {
+            return new PublicAddress(reader.GetString());
+        }
+
+        public override void Write(Utf8JsonWriter writer, PublicAddress value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+
+    class HexKey64Converter : JsonConverter<HexKey64>
+    {
+        public override HexKey64? Read(ref Utf8JsonReader reader, System.Type typeToConvert, JsonSerializerOptions options)
+        {
+            return new HexKey64(reader.GetString());
+        }
+
+        public override void Write(Utf8JsonWriter writer, HexKey64 value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+
+    class BigIntegerConverter : JsonConverter<BigInteger>
+    {
+        public override BigInteger Read(ref Utf8JsonReader reader, System.Type typeToConvert, JsonSerializerOptions options)
+        {
+            return BigInteger.Parse(reader.GetString());
+        }
+
+        public override void Write(Utf8JsonWriter writer, BigInteger value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString());
         }
     }
 }
---
+++ Nandro/NanoApiClient.cs
@@ -0,0 +1,41 @@
+﻿using DotNano.RpcApi;
+using System;
+using System.Net.Http;
+
+namespace Nandro
+{
+    class NanoApiClient : IDisposable
+    {
+        private HttpClient _apiClient;
+        private string _url;
+
+        public NanoApiClient(string url)
+        {
+            _apiClient = new HttpClient();
+            _url = url;
+        }
+
+        public string? AccountHistory(string account)
+        {
+            try
+            {
+                var url = $"{_url}/?action=account_history&account={account}&count=10";
+                var result = _apiClient.GetAsync(url).Result;
+
+                if (result.IsSuccessStatusCode)
+                    return result.Content.ReadAsStringAsync().Result;
+                else
+                    return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public void Dispose()
+        {
+            _apiClient.Dispose();
+        }
+    }
+}
